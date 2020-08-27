cd6a733b59f7ac3807072821e1b70cb3f0ce028e
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2020-08-27 16:16:11 +0100

    :tada: Added searching for squads a player has

---
+++ src/server/units.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+let squadData = JSON.parse(fs.readFileSync('./server/units/squads.json'));
+
+const getSquads = (unitsList) => {
+    const unitsMap = new Map();
+    const squads = [];
+    // Convert the unit list to a map so that lookups will be O(1)
+    unitsList.forEach((unit) => {
+        unitsMap.set(unit.defId, unit);
+    });
+    squadData.forEach(squad => {
+        const unitArr = [];
+        squad.units.forEach((unit) => {
+            // Will return undefined if they don't have the unit
+            let playerUnit = unitsMap.get(unit);
+            // Undefined is fasly
+            if(playerUnit){
+                unitArr.push(playerUnit);
+            }
+        })
+        if(unitArr.length >= 5){
+            squads.push({title: squad.title, units: unitArr});
+        }
+    })
+}
+
+const convertName = (name) => {
+    return name;
+}
+
+module.exports = {
+    getSquads: getSquads,
+}
