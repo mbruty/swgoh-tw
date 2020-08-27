c64fe1e6e157ca9ba4ce035313f2843fbf668a20
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2020-08-27 11:02:50 +0100

    :sparkles: Sett-up express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,29 @@
 const express = require('express');
+require('dotenv').config();
+const app = express();
+var cors = require('cors')
+var bodyParser = require('body-parser');
+
+// Cors
+const whitelist = ['http://localhost:5000', 'http://localhost:3000', 'http://bruty.net'];
+
+const corsOptions = {
+    origin: (origin, callback)  => {
+        if(whitelist.indexOf(origin) !== -1) {
+            callback(null,true);
+        }
+        else{
+            callback('Route not allowed');
+        }
+    },
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+// Routes
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    
+})
