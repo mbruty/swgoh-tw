57c9c3e660233b12e1d28e4f8f4dfc696bec8fe9
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2020-08-28 18:50:44 +0100

    :sparkles: Merge sort on a player's roster

---
+++ src/server/fetchData/sortToons.js
@@ -0,0 +1,26 @@
+module.exports = (data) => {
+    if(data.length <= 1) return data;
+    let dataCp = {...data};
+    mergeSort(data, 0, data.length - 1, dataCp);
+    return data.reverse();
+}
+
+function mergeSort (main, start, end, copy) {
+    if(start === end) return;
+    const mid = Math.floor((start + end) / 2);
+    mergeSort(copy, start, mid, main);
+    mergeSort(copy, mid + 1, end, main);
+    merge(main, start, mid, end, copy);
+}
+
+function merge(main, start, mid, end, copy) {
+    let k = start;
+    let i = start;
+    let j = mid + 1;
+    while (i <= mid && j <= end){
+        if (copy[i].power <= copy[j].power) main[k++] = copy[i++];
+        else main[k++] = copy[j++];
+    }
+    while (i <= mid) main[k++] = copy[i++];
+    while(j <= end) main[k++] = copy[j++];
+}
