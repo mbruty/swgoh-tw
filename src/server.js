7a4923746816c9d5628a4654dc113aa5facb9644
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2020-08-28 18:37:50 +0100

    :truck: Moving swapi logic in to fetchData

--- src/server.js
+++ src/server.js
@@ -1,48 +1,9 @@
 const express = require('express');
+const start = require('./server/fetchData');
 require('dotenv').config();
 const app = express();
 var cors = require('cors')
 var bodyParser = require('body-parser');
-const ApiSwgohHelp = require('api-swgoh-help');
-const Cache = require('./server/cache.js');
-const codes = [647793576, 711217969 ];
-
-const dataCache = new Cache();
-
-let swapi = undefined;
-// API Login
-const login = () => {
-    swapi = new ApiSwgohHelp({
-        "username":process.env.API_UNAME,
-        "password":process.env.API_PASSWORD
-    });
-    return new Promise(async (resolve, reject) => {
-        swapi.connect()
-        .then(res => resolve(res))
-        .catch(err => reject(err));
-
-    })
-}
-
-const fetchGuildData = async (allycodes) => {
-    const payload = {
-        allycodes,
-        collection: "guildExchangeItemList"
-    }
-    let  { result, error, warning }  = await swapi.fetchGuild( payload );
-
-    //Result is an array of guild data
-    //Example object returned see guild_sample_result.json
-    if(error) {
-        console.error(error);
-    }
-    if(warning) {
-        console.warn(warning);
-    }
-    else{
-        dataCache.addGuild(result);
-    }
-}
 // Cors 
 
 const whitelist = ['http://localhost:5000', 'http://localhost:3000', 'http://bruty.net'];
@@ -69,11 +30,13 @@ app.get('/:id', (req, res) => {
 
 //Start services
 console.clear();
-login()
-//Fetch the data for the tracked guilds
-.then(() => fetchGuildData(codes))
-.catch(err => console.error(err));
+start();
 const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
+
+// Log any unhandled erors
+process.on('unhandledRejection', err => {
+    console.log(`Unhandled error: ${err}`);
+})
