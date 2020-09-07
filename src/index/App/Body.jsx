1f60803140ea2aca977f2e18c1a3a2a9d866216c
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2020-09-07 21:06:28 +0100

    :art: Added skeleton to loading

--- src/index/App/Body.jsx
+++ src/index/App/Body.jsx
@@ -4,6 +4,7 @@ import Squads from "./Body/Squads";
 import Paper from "@material-ui/core/Paper";
 import { LinearProgress, Box, Typography } from "@material-ui/core";
 import SearchForm from "./Body/SearchForm";
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const Body = (props) => {
 	const [data, setData] = useState();
@@ -55,6 +56,14 @@ const Body = (props) => {
 				<Paper elevation={3} className="Paper">
 					<h1>{fetchState.message}</h1>
 					<LinearProgressWithLabel value={fetchState.progress} />
+					<Skeleton animation="wave" variant="text" width={450} height={60} />
+					<Skeleton animation="wave" variant="text" width={650} height={30} />
+					<Skeleton style={{marginLeft: '25px'}} animation="wave" variant="text" width={150} height={25} />
+					<Skeleton style={{marginLeft: '25px'}} animation="wave" variant="text" width={150} height={25} />
+					<Skeleton animation="wave" variant="text" width={'100%'} height={80} />
+					<Skeleton animation="wave" variant="text" width={'100%'} height={80} />
+					<Skeleton animation="wave" variant="text" width={'100%'} height={80} />
+					<Skeleton animation="wave" variant="text" width={'100%'} height={80} />
 				</Paper>
 			</div>
 		);
