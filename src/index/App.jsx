010ee757cd5407769c071483001fcc9c7208566b
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2020-09-10 16:14:02 +0100

    Converted in to spaces

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -9,92 +9,92 @@ import { Paper, Tabs, Tab, makeStyles, AppBar, Toolbar, IconButton, Typography, 
 
 let firstRender = true;
 const theme = createMuiTheme({
-	palette: {
-		type: "dark",
-		primary: {
-			main: "#25c5d8",
-		},
-		secondary: {
-			main: "#64dd17",
-		},
-	},
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#25c5d8",
+    },
+    secondary: {
+      main: "#64dd17",
+    },
+  },
 });
 const useStyles = makeStyles({
-	root: {
-		flexGrow: 1,
-		backgroundColor: "#0095a8",
-		transform: "translateY(2px)",
-	},
+  root: {
+    flexGrow: 1,
+    backgroundColor: "#0095a8",
+    transform: "translateY(2px)",
+  },
 });
 
 const App = () => {
-	// Get the allycodes from the url
-	let params = window.location.search.substring(1);
-	if (params !== "" && firstRender) {
-		fetchData(params);
-		firstRender = false;
-	}
-	const classes = useStyles();
-	const [value, setValue] = React.useState(0);
-	const handleChange = (event, newValue) => {
-		setValue(newValue);
-		console.log(newValue);
-	};
-	if (value === 0) {
-		return (
-			<div className="App">
-				<ThemeProvider theme={theme}>
-					<AppBar position="sticky">
-						<Toolbar>
-							<Typography variant="h6" className={classes.title}>
-								SWGOH Inspector
-							</Typography>
-						</Toolbar>
-					<Paper className={classes.root}>
-						<Tabs
-							value={value}
-							onChange={handleChange}
-							indicatorColor="primary"
-							textColor="primary"
-							centered
-						>
-							<Tab label="Territory War Inspector" />
-							<Tab label="Guild Inspector" />
-						</Tabs>
-					</Paper>
-					</AppBar>
-					<TWBody codes={params.substring()} />
-				</ThemeProvider>
-			</div>
-		);
-	} else if (value === 1) {
-		return (
-			<div className="App">
-				<ThemeProvider theme={theme}>
+  // Get the allycodes from the url
+  let params = window.location.search.substring(1);
+  if (params !== "" && firstRender) {
+    fetchData(params);
+    firstRender = false;
+  }
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    console.log(newValue);
+  };
+  if (value === 0) {
+    return (
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <AppBar position="sticky">
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                SWGOH Inspector
+              </Typography>
+            </Toolbar>
+          <Paper className={classes.root}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+            >
+              <Tab label="Territory War Inspector" />
+              <Tab label="Guild Inspector" />
+            </Tabs>
+          </Paper>
+          </AppBar>
+          <TWBody codes={params.substring()} />
+        </ThemeProvider>
+      </div>
+    );
+  } else if (value === 1) {
+    return (
+      <div className="App">
+        <ThemeProvider theme={theme}>
         <AppBar position="sticky">
-						<Toolbar>
-							<Typography variant="h6" className={classes.title}>
-								SWGOH Inspector
-							</Typography>
-						</Toolbar>
-					<Paper className={classes.root}>
-						<Tabs
-							value={value}
-							onChange={handleChange}
-							indicatorColor="primary"
-							textColor="primary"
-							centered
-						>
-							<Tab label="Territory War Inspector" />
-							<Tab label="Guild Inspector" />
-						</Tabs>
-					</Paper>
-					</AppBar>
-					<GuildBody codes={params.substring()} />
-				</ThemeProvider>
-			</div>
-		);
-	}
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                SWGOH Inspector
+              </Typography>
+            </Toolbar>
+          <Paper className={classes.root}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+            >
+              <Tab label="Territory War Inspector" />
+              <Tab label="Guild Inspector" />
+            </Tabs>
+          </Paper>
+          </AppBar>
+          <GuildBody codes={params.substring()} />
+        </ThemeProvider>
+      </div>
+    );
+  }
 };
 
 export default App;
